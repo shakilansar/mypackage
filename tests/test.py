e321ed721ffd2fa62729e4fafac44852bc5127b0
Shakil Ahmad <shakilansar@outlook.com>
2022-09-11 23:09:28 +0200

    added main to run from test

--- tests/test.py
+++ tests/test.py
@@ -8,3 +8,8 @@ def test_top_n():
     assert myModule.top_n([8, 3, 2, 7, 4], 3) == [8, 7, 4], 'incorrect'
     assert myModule.top_n([10, 1, 12, 9, 2], 2) == [12, 10], 'incorrect'
     assert myModule.top_n([1, 2, 3, 4, 5], 5) == [5, 4, 3, 2, 1], 'incorrect'
+    x = myModule.top_n([8, 3, 2, 7, 4], 3)
+    print(x)
+
+if __name__ =='main':
+    test_top_n()
